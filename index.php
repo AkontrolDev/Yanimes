1af7ef6e9c79c111a67119282a2292d60c6a0e1a
AkontrolDev <mendesgabrielsouza84@gmail.com>
2024-11-28 10:27:00 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yanimes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1 class="logo">Yanimes</h1>
+            <nav>
+                <ul>
+                    <li><a href="#inicio">Início</a></li>
+                    <li><a href="animes.html">Animes</a></li>
+                    <li><a href="#">Gêneros</a></li>
+                    <li><a href="#">Favoritos</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <!-- Seção Hero -->
+        <section class="hero">
+            <div class="overlay">
+                <h1>Bem-vindo ao Yanimes</h1>
+                <p>Descubra, explore e se apaixone pelos melhores animes do universo!</p>
+                <a href="#inicio" class="cta">Explore Agora</a>
+            </div>
+        </section>
+
+        <!-- Aba Início -->
+        <section id="inicio" class="anime-cards">
+            <div class="container">
+                <h2>Animes em Destaque</h2>
+                <div class="card-grid">
+                    <!-- Card 1 -->
+                    <div class="card">
+                        <img src="https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=480,height=720/catalog/crunchyroll/5e7f533c3b4f46244ca228af62e83dfa.jpg" alt="Anime 1">
+                        <h3>Solo Leveling</h3>
+                        <p>Ação, Aventura, Fantasia</p>
+                    </div>
+                    <!-- Card 2 -->
+                    <div class="card">
+                        <img src="https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=480,height=720/catalog/crunchyroll/b5621ff1277ed6ad1006b0c6f14900bb.jpg" alt="Anime 2">
+                        <h3>Blue Lock</h3>
+                        <p>	Esporte, Shōnen</p>
+                    </div>
+                    <!-- Card 3 -->
+                    <div class="card">
+                        <img src="https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=480,height=720/catalog/crunchyroll/cbb55a6382682bf71e91f685c6473c5a.jpg" alt="Anime 3">
+                        <h3>Hunter x Hunter</h3>
+                        <p>Ação, Aventura, Fantasia</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2024 Yanimes - Todos os direitos reservados.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
